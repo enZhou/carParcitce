cef34af82e3a4c3bf3328ec4de08bbbe79da1639
EnZhou <758495585@qq.com>
2019-08-02 20:05:26 +0800

    首页

--- src/views/questionBank/questionBank.vue
+++ src/views/questionBank/questionBank.vue
@@ -1,48 +1,90 @@
 <template>
   <div class="questionBank">
-    <!-- 答题/背题模式切换 -->
-    <commonNav :active="active" :showType="showType" @changeModal="changeType">
-      <template slot="content">
-        <div v-show="active === 'answer'">
-          答题模式内容
+    <div class="bank-title">
+      <p>选择题库类型</p>
+    </div>
+    <div class="bank-content">
+      <div class="cnt_tab">
+        <div class="cnt_tab_item" @click="changeType(0)">
+          <span :class="bankType===0?'active':''">科目一</span>
         </div>
-        <div v-show="active === 'recite'">
-          背题模式内容
+        <div class="cnt_tab_item" @click="changeType(1)">
+          <span :class="bankType===1?'active':''">科目四</span>
         </div>
-      </template>
-    </commonNav>
-    <p class="bank-title">选择题库类型</p>
+      </div>
+      <div class="cnt_body_one" v-if="bankType==0">科目一</div>
+      <div class="cnt_body_four" v-if="bankType==1">科目四</div>
+    </div>
+
     <stuFooter></stuFooter>
   </div>
 </template>
 <script>
 import stuFooter from "../../components/stuFooter";
-import newHead from "../../components/newHead";
 // import commonNav from "../../components/commonNav.vue";
 export default {
-  components: { newHead, stuFooter },
+  components: { stuFooter },
   data() {
     return {
       title: "驾考题库",
-      active: "answer",//默认答题模式
-      showType:'tab'
+      bankType: 0, //科目类型
+      showType: "tab"
     };
   },
   methods: {
     // 选择模式
     changeType(val) {
-      let self = this;
-      self.active = val;
+      let vm = this;
+      vm.bankType = val;
     }
   }
 };
 </script>
 <style scoped lang="scss">
-.questionBank{
-  .bank-title{
-    font-size: 1rem;
-    padding: 1rem;
-    background-color: #1B82D1;
+.questionBank {
+  .bank-title {
+    padding: 0.3rem;
+    background-color: #1b82d1;
+    text-align: center;
+    p {
+      font-size: 0.4rem;
+      color: #fff;
+    }
+  }
+  .bank-content {
+    .cnt_tab {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+      border-bottom: 1px solid #ccc;
+      .cnt_tab_item {
+        width: 50%;
+        text-align: center;
+        span {
+          text-align: center;
+          position: relative;
+          display: inline-block;
+          padding: 0.26rem;
+          font-size: 0.28rem;
+          font-weight: bold;
+          color: #929292;
+        }
+        .active {
+          color: #0f0f0f;
+
+          &:before {
+            content: "";
+            position: absolute;
+            top: 98.5%;
+            left: 0;
+            border: 1px solid red;
+            border-radius: 2px;
+            width: 100%;
+          }
+        }
+      }
+    }
   }
 }
 </style>
---
+++ src/views/questionBank/subjectPage/subjectOne.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="subjectOne">
+        科目1内容
+    </div>
+</template>
